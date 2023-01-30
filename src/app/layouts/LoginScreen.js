30f06b16d7069225720a1b3e07e64b1560969237
programmerrafi <mdrafiprogrammer@gmail.com>
2023-01-30 20:26:23 +0600

    again start netflix clone

--- src/app/layouts/LoginScreen.js
+++ src/app/layouts/LoginScreen.js
@@ -17,9 +17,9 @@ function LoginScreen() {
         {/* Header */}
         <div className=" flex items-center justify-between w-full">
           <img
-            className="w-[180px] object-contain pl-[20px] cursor-pointer"
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-            alt=""
+            className="w-[180px] object-contain pl-[20px] cursor-pointer z-20"
+            src="images/net.png"
+            alt="netflix"
           />
           {!signIn && (
             <button
