4eeefce1d9c1e80e0676530bdcc95676e08abcb0
programmerrafi <mdrafiprogrammer@gmail.com>
2023-02-05 04:27:41 +0600

    Finally netflix done

--- src/app/Components/HoverYoutube.js
+++ src/app/Components/HoverYoutube.js
@@ -8,7 +8,7 @@ function HoverYoutube({ trailerUrl, onClick }) {
       width="100%"
       height="100%"
       playing
-      muted={false}
+      muted={true}
       onPause={onClick}
     />
   );
