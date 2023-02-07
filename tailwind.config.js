a9f29bcca1bd05b3104093cea16d7f35b69f2c12
programmerrafi <mdrafiprogrammer@gmail.com>
2023-02-07 22:26:07 +0600

    api key add in env file

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwind-scrollbar-hide"),
-    require("tailwind-scrollbar"),
-    require("@tailwindcss/line-clamp"),
-    require("tailwindcss-textshadow"),
-  ],
+  plugins: [],
 };
